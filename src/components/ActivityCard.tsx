968331c35e40efdaa3213da8ec19355dba036289
ricky80l <98959778+ricky80l@users.noreply.github.com>
2026-07-04 00:08:51 +0200

    fix: remove unused React import causing build failure

--- src/components/ActivityCard.tsx
+++ src/components/ActivityCard.tsx
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect } from 'react';
 import { Activity, getCategoryLabel, LEVEL_LABELS, getDistanceKm } from '../data/mockActivities';
 
 interface ActivityCardProps {
